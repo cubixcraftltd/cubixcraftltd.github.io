704d6ba999674e6d149ce64afe3d2846eb420c8b
Akash Mishra <114241102+akashmishrahrmc@users.noreply.github.com>
2023-10-15 20:31:30 +0100

    Remvoing out

--- out/_next/static/_N0VQzRDRCtpdBOqWc4hw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/735-287576ee6e020cfa.js","static/chunks/pages/index-7836fb331ee2362f.js"],"/_error":["static/chunks/pages/_error-4afcb85b7c260fd3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
